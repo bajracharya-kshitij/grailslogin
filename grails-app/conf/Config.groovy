65537e10b698aad80acf3d6d3a349b9e3dd16f3a
Kshitij Bajracharya <bajracharya.kshitij@gmail.com>
2017-01-07 12:11:15 +0545

    Create User, Role and UserRole domains with spring security

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -119,3 +119,20 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'np.com.grailslogin.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'np.com.grailslogin.UserRole'
+grails.plugin.springsecurity.authority.className = 'np.com.grailslogin.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                ['permitAll'],
+	'/index':           ['permitAll'],
+	'/index.gsp':       ['permitAll'],
+	'/assets/**':       ['permitAll'],
+	'/**/js/**':        ['permitAll'],
+	'/**/css/**':       ['permitAll'],
+	'/**/images/**':    ['permitAll'],
+	'/**/favicon.ico':  ['permitAll']
+]
+
